c6e94c0c2e34c98d1d6ccd0ed66168e4cce32f39
Artur Afonso <artuhafonso@gmail.com>
2025-11-19 21:02:26 -0300

    fix(database): corrige retorno do método insertTag
    - Altera retorno de Future<int> para Future<String>
    - Retorna tag.id.value após inserção
    - Gera código com build_runner
    
    Ref: feature/drift-database

---
+++ lib/data/database/daos/tags_dao.g.dart
@@ -0,0 +1,10 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'tags_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$TagsDaoMixin on DatabaseAccessor<AppDatabase> {
+  $TagsTable get tags => attachedDatabase.tags;
+  $NotesTable get notes => attachedDatabase.notes;
+  $NoteTagsTable get noteTags => attachedDatabase.noteTags;
+}
